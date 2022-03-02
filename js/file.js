0d06bffe090c7bcf7b677c63541d392279e6bb6f
Kisavi <deniskisavi@gmail.com>
2022-03-02 23:13:01 +0300

    Declare and assign global variables

---
+++ js/file.js
@@ -0,0 +1,5 @@
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let femaleNames = ["Akosue", "Adwoa", "Abenaa", "Akue", "Yaa", "Afua", "Ama"]
+let genders = document.getElementsByClassName("gender")
