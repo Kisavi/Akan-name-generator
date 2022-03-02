9bc333d107fcbfe7ef89dabe22e1ed0e350afef0
Kisavi <deniskisavi@gmail.com>
2022-03-02 23:15:20 +0300

    add function that is called on submit button click

--- js/file.js
+++ js/file.js
@@ -1,5 +1,24 @@
-
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femaleNames = ["Akosue", "Adwoa", "Abenaa", "Akue", "Yaa", "Afua", "Ama"]
 let genders = document.getElementsByClassName("gender")
+
+function displayDate() {
+
+    let x = document.forms["myForm"]["date"].value;
+    let p = document.forms["myForm"]["sex"].value;
+
+    if (x == "") {
+        alert("This is not a valid date format!!!");
+    }
+    //console.log(x)
+    //console.log(p)
+    checkDay(x);
+    displayContainer();
+    for (let gender of genders) {
+        if (gender.checked) {
+            //console.log(gender.value)
+            return gender.value;
+        }
+    }
+}
