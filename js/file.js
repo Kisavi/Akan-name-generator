e74499e92dbc1f6d10f83023fd90427825a7d73d
Kisavi <deniskisavi@gmail.com>
2022-03-04 15:26:06 +0300

    add comments to ease readability

--- js/file.js
+++ js/file.js
@@ -1,17 +1,17 @@
+//declaring initial variables
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femaleNames = ["Akosue", "Adwoa", "Abenaa", "Akue", "Yaa", "Afua", "Ama"]
 let genders = document.getElementsByClassName("gender")
 
 function displayDate() {
-
+//declaring more variables
     let userDateInput = document.forms["myForm"]["date"].value;
-    let userSexInput = document.forms["myForm"]["sex"].value;
-    let z = document.getElementById("message")
+    let outputParagraph = document.getElementById("message")
     let isValidDate = Date.parse(userDateInput);
     let date = new Date(isValidDate)
     let index = date.getDay();
-
+//conditions to be checked in order to display the correct Akan details
     if (x == "" && document.getElementById("flexRadioDefault1").checked) {
         alert("This is not a valid date format!!!");
         return false;
@@ -19,12 +19,12 @@ function displayDate() {
         alert("This is not a valid date format!!")
         return false;
     } else if (x !== "" && document.getElementById("flexRadioDefault1").checked) {
-        z.innerHTML = (`You were born on ${days[index]} and your Akan name is ${maleNames[index]}.`)
-    } else {z.innerHTML = (`You were born on ${days[index]} and your Akan name is ${femaleNames[index]}.`)}
+        outputParagraph.innerHTML = (`You were born on ${days[index]} and your Akan name is ${maleNames[index]}.`)
+    } else {outputParagraph.innerHTML = (`You were born on ${days[index]} and your Akan name is ${femaleNames[index]}.`)}
     
     displayContainer();
 }
-
+//funtion for hiding the form container and display the result's paragraph container.
 function displayContainer() {
     let x = document.getElementById("container1")
     let y = document.getElementById("container2")
